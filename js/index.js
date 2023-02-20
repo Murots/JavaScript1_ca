c48ad77194f2088bb769fe1b11c287d11411d07b
Aleksander Storum <alesto95654@stud.noroff.no>
2023-02-20 11:20:29 +0100

    Skipping Svalbard from API and adding try/catch

--- js/index.js
+++ js/index.js
@@ -67,26 +67,35 @@
 
 
 const countryContainer = document.querySelector(".europe");
-const url = "https://restcountries.com/v3.1/region/europe";
+const url = "https://restcountries.com/v3.1/region/euroope";
 
 async function fetchCountries() {
-    const response = await fetch(url);
-    const countryList = await response.json();
-    console.log(countryList);
+    try {
+        const response = await fetch(url);
+        const countryList = await response.json();
+        console.log(countryList);
 
-    countryContainer.innerHTML = "";
-
+        countryContainer.innerHTML = "";
     
-    for (let i = 0; i < countryList.length; i++) {
-        const countryName = countryList[i].name.common;
-        const officialName = countryList[i].name.official
-        const flag = countryList[i].flags.png
-        countryContainer.innerHTML += `<a class="single" href="details.html?id=${countryName}">
-                                        <h2>${countryName}</h2>
-                                        <img src=${flag} class="flag"></img>
-                                        <p>Official: ${officialName}</p>
-                                        </a>`;
+        for (let i = 0; i < countryList.length; i++) {
+            const countryName = countryList[i].name.common;
+
+            if (countryName === "Svalbard and Jan Mayen") {
+                continue;
+            }
+
+            const officialName = countryList[i].name.official
+            const flag = countryList[i].flags.png
+            countryContainer.innerHTML += `<a class="single" href="details.html?id=${countryName}">
+                                            <h2>${countryName}</h2>
+                                            <img src=${flag} class="flag"></img>
+                                            <p>Official: ${officialName}</p>
+                                            </a>`;
+        }
+    }  catch (error) {
+        console.log(error);
+        countryContainer.innerHTML = errorMessage("Could not fetch data. Please try again later.");
     }
 }
 
-fetchCountries();
+fetchCountries()
